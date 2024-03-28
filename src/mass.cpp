c5b4bc45a800ae0f4682de9397bd01c3ac066266
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-03-28 18:31:09 +0100

    impl getter and setter

--- src/mass.cpp
+++ src/mass.cpp
@@ -4,32 +4,45 @@
 #include <array>
 #include <iostream>
 
-#include "physics.hpp"
-
+#include "mass.hpp"
 
 const int NUMBER_MASSES = 3;
 
-class SystemOfMasses
-{
-    std::array<Mass &, NUMBER_MASSES> masses;
-
-public:
-    SystemOfMasses(std::array<Mass &, NUMBER_MASSES> masses)
-    {
-        this->masses = masses;
-    };
-
-private:
-    double compute_acceleration(Mass &mass){
-
-    };
-};
-
-physics::Mass(std::string name, double mass, std::array<double, 3> position, std::array<double, 3> velocity, std::array<double, 3> acceleration)
+Mass::Mass(std::string name, double mass, std::array<double, 3> position, std::array<double, 3> velocity, std::array<double, 3> acceleration)
 {
     this->name = name;
     this->mass = mass;
     this->position = position;
     this->velocity = velocity;
+    this->acceleration = acceleration;
+};
+
+std::array<double, 3> Mass::getPosition()
+{
+    return this->position;
+};
+
+void Mass::setPosition(std::array<double, 3> position)
+{
+    this->position = position;
+};
+
+std::array<double, 3> Mass::getVelocity()
+{
+    return this->velocity;
+};
+
+void Mass::setVelocity(std::array<double, 3> velocity)
+{
+    this->velocity = velocity;
+};
+
+std::array<double, 3> Mass::getAcceleration()
+{
+    return this->acceleration;
+};
+
+void Mass::setAcceleration(std::array<double, 3> acceleration)
+{
     this->acceleration = acceleration;
 };
