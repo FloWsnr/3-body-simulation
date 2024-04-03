a5f8446219bbb838049f43ffe122611955c3ecef
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-04-03 18:38:12 +0200

    add command line args to app

--- apps/main.cpp
+++ apps/main.cpp
@@ -5,10 +5,18 @@
 #include "body.hpp"
 #include "n_body_system.hpp"
 #include "simulation.hpp"
+#include "com_parser.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    ConfigReader config("/home/flwi/Coding/3-body-simulation/apps/default_config.json");
+    std::cout << "3-body simulation" << std::endl;
+    std::cout << "-----------------" << std::endl;
+
+    ComParser com_parser(argc, argv);
+    Arguments arguments = com_parser.parse();
+
+
+    ConfigReader config(arguments.config_path);
     std::vector<Body> bodies = config.getBodies();
     double duration = config.getDuration();
     double timestep = config.getTimestep();
