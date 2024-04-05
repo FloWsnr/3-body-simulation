ddba52d51df7b8791d2935f8c8070f1aaf84b960
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-04-05 11:56:11 +0200

    add logEvery to main

--- apps/main.cpp
+++ apps/main.cpp
@@ -19,23 +19,28 @@ int main(int argc, char* argv[])
 
     // Create logger
     const std::string log_file = config.getLogFile();
+    const double logEvery = config.getLogEvery();
     Logger logger = Logger(log_file, arguments.verbose);
 
     std::vector<Body> bodies = config.getBodies();
     NBodySystem n_body_system(bodies);
+
+    logger.logMessage("Initial configuration", 0);
+    logger.logMessage("--------------------------", 0);
     logger.logNBodySystem(n_body_system, 1);
 
+    logger.logMessage("--------------------------", 0);
     logger.logMessage("Starting N body simulation", 0);
     logger.logMessage("--------------------------", 0);
 
-    Simulation sim(n_body_system, logger);
+    Simulation sim(n_body_system, &logger);
 
     double duration = config.getDuration();
     double timestep = config.getTimestep();
 
-    logger.logMessage("Start simulation:", 0);
-    sim.simulate(timestep, duration);
+    logger.logMessage("Start simulation:", 1);
+    sim.simulate(timestep, duration, logEvery);
 
-    logger.logMessage("Simulation complete", 0);
+    logger.logMessage("Simulation complete", 1);
     return 0;
 };
